db6cdc657eefa8c5f2bb928799d879d5cf89b6f6
273dan <dankirby273@gmail.com>
2025-10-04 15:28:42 +0100

    Added telescope-ui-select to nvim

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,16 @@
 return {
-    'nvim-telescope/telescope.nvim', 
+    'nvim-telescope/telescope.nvim',
     tag='0.1.8',
-    dependencies = {'nvim-lua/plenary.nvim'}
+    dependencies = {'nvim-lua/plenary.nvim'},
+    extensions = {
+        ["ui-select"] = {
+              require("telescope.themes").get_dropdown {
+                -- even more opts
+              }
+          }
+    },
+    config = function()
+        require("telescope").load_extension("ui-select")
+    end
+
 }
